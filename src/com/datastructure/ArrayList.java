2bddbfc2d27dc1f4b914d10e5b9dac9bb79d4ab6
Xia Zhao <zhaoxia9406@gmail.com>
2022-06-25 20:53:24 -0800

    ArrayList

---
+++ src/com/datastructure/ArrayList.java
@@ -0,0 +1,127 @@
+package com.datastructure;
+
+public class ArrayList<E> {
+
+    private static final int DEFAULT_SIZE = 16;
+    private static final int ELEMENT_NOT_FOUND = -1;
+
+    private int size;
+    private E[] elements;
+
+    public ArrayList(int capacity) {
+        capacity = capacity >= size ? capacity : size;
+        elements = (E[]) new Object[capacity];
+    }
+
+    public ArrayList() {
+        this(DEFAULT_SIZE);
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean contains(E element) {
+        return indexOf(element) != ELEMENT_NOT_FOUND;
+    }
+
+    public void add(E element) {
+        add(size, element);
+    }
+
+    public E get(int index) {
+        rangeCheck(index);
+        return elements[index];
+    }
+
+    public E set(int index, E element) {
+        rangeCheck(index);
+        E old = elements[index];
+        elements[index] = element;
+        return old;
+    }
+
+    public void add(int index, E element) {
+        rangeCheckForAdd(index);
+        ensureCapacity(size + 1);
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        size++;
+    }
+
+    public E remove(int index) {
+        rangeCheck(index);
+        E old = elements[index];
+        for (int i = index + 1; i < size; i++) {
+            elements[i - 1] = elements[i];
+        }
+        elements[--size] = null;
+        return old;
+    }
+
+    public int indexOf(E element) {
+        if (element == null) {  // 1
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) return i;
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (element.equals(elements[i])) return i; // n
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    private void ensureCapacity(int capacity) {
+        if (capacity > size) return;
+        capacity = capacity * 2;
+        E[] oldElements = elements;
+        elements = (E[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            elements[i] = oldElements[i];
+        }
+    }
+
+    private void outOfBounds(int index) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    private void rangeCheck(int index) {
+        if (index < 0 || index >= size) {
+            outOfBounds(index);
+        }
+    }
+
+    private void rangeCheckForAdd(int index) {
+        if (index < 0 || index > size) {
+            outOfBounds(index);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        string.append("size=").append(size).append(", [");
+        for (int i = 0; i < size; i++) {
+            if (i != 0) {
+                string.append(", ");
+            }
+            string.append(elements[i]);
+        }
+        string.append("]");
+        return string.toString();
+    }
+}
